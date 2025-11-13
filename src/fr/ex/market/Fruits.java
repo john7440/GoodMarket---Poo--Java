cdbabc5785ced60292b07950cdd02cda528767f2
Jonathan Maier <jonathan.maier@fms-ea.com>
2025-11-13 15:42:16 +0100

    ajout d'une méthode toString()

--- src/fr/ex/market/Fruits.java
+++ src/fr/ex/market/Fruits.java
@@ -13,7 +13,16 @@ public class Fruits extends Product {
 	@Override
 	public void calculateExpirationDate() {
 		// TODO Auto-generated method stub
-		
+	}
+	
+	@Override
+	public String toString() {
+		return "Fruit: " + this.getName() + "\n" +
+			   "Stock: " + this.getStockQuantity() + " " + this.getUnit() + "\n"+
+			   "Prix: " + this.getUnitPrice() + "/" + this.getUnit() + "\n" +
+			   "Date de récolte: " + this.getPickingDate() + "\n" +
+			   "Duré de conservation: " + this.getShelfLifeDays() + " jours\n";
+				
 	}
 
 }
